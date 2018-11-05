22f15c48517f9b54f592824a16b6ef8931ba9c55
Owen J Sullivan <multiojuice@gmail.com>
2018-11-04 16:47:52 -0800

    Added some tiles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 
 import {
-  LargeTitle, PageWrapper, LargeContainer, MediumTitle, MediumContainer, WholePage
+  PageWrapper,
+  MediumTitle,
+  MediumContainer,
+  WholePage,
+  WholePageTitle
 } from './StyledComponents';
 
 export default () => {
   return (
-    <PageWrapper>
-      <WholePage />
-      <LargeContainer colors={['#206ba4', '#Ebf4fa']}>
-        <LargeTitle>Owen Sullivan</LargeTitle>
-      </LargeContainer>
-      <MediumContainer colors={['#Bbd9ee', '#C0c0c0']}>
-        <MediumTitle>Projects</MediumTitle>
-      </MediumContainer>
-      <MediumContainer colors={['#Bbd9ee', '#C0c0c0']}>
-        <MediumTitle>Experience</MediumTitle>
-      </MediumContainer>
+    <PageWrapper clear>
+      <WholePage>
+        <WholePageTitle>Owen Sullivan</WholePageTitle>
+      </WholePage>
+      <PageWrapper ehh>
+        <MediumContainer colors={['#Bbd9ee', '#C0c0c0']}>
+          <MediumTitle>Projects</MediumTitle>
+        </MediumContainer>
+        <MediumContainer colors={['#Bbd9ee', '#C0c0c0']}>
+          <MediumTitle>Experience</MediumTitle>
+        </MediumContainer>
+        <MediumContainer colors={['#Bbd9ee', '#C0c0c0']}>
+          <MediumTitle>Me</MediumTitle>
+        </MediumContainer>
+        <MediumContainer colors={['#Bbd9ee', '#C0c0c0']}>
+          <MediumTitle>Day to Day</MediumTitle>
+        </MediumContainer>
+      </PageWrapper>
     </PageWrapper>
   );
 }
--- src/pages/Home/StyledComponents.js
+++ src/pages/Home/StyledComponents.js
@@ -7,28 +7,46 @@ export const PageWrapper = styled.div`
   align-items: center;
   width: 100%;
   height: 100%;
+  background-color: ${({clear}) => clear ? '' : '#f7f7f7'}
+  z-index: ${({ehh}) => ehh ? 1 : ''}
 `;
 
 export const WholePage = styled.div`
   height: 100vh;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
+export const WholePageTitle = styled.h1`
+  color: white;
+  padding: 30px;
+  font-size: 48px;
+  position: fixed;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border-radius: 15px;
+  background-image: radial-gradient(black, transparent);`;
+
 export const LargeContainer = styled.div`
-  width: 80%;
+  width: 100%;
   height: 600px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   border-radius: 15px;
-  background-image: ${({colors}) => `linear-gradient(to bottom right, ${colors[0]}, ${colors[1]})`};
+  background-color: #f7f7f7;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   transition: box-shadow 1s;
   :hover {
     box-shadow: 0 12px 20px 0 rgba(0,0,0,0.2);
   }
-  margin-bottom: 40px;
 `;
 
 export const LargeTitle = styled.h1`
@@ -36,18 +54,15 @@ export const LargeTitle = styled.h1`
 `;
 
 export const MediumContainer = styled.div`
-  width: 40%;
+  width: 50%;
   height: 400px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border-radius: 15px;
-  background-image: ${({colors}) => colors ? `linear-gradient(to bottom right, ${colors[0]}, ${colors[1]})` : ''};
+  background-color: #f7f7f7;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-  margin-bottom: 40px;
   transition: box-shadow 1s;
-  
   :hover {
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
   }
