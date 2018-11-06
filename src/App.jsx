d90358339a55dd894d9e28ae197ea5502f6c189d
Owen J Sullivan <multiojuice@gmail.com>
2018-11-05 19:15:36 -0800

    Setup the beginning of the projects page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,16 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import {
-  Home
+  Home,
+  Projects
 } from './pages';
-
-const Projects = () => {
-  return <div>Projects</div>
-}
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/projects' component={Projects}/>
         <Route exact path='/projects/:id' component={Projects}/>
       </Switch>
       );
---
+++ src/pages/Projects/StyledComponents.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+`;
+
+export const PostWrapper = styled.div`
+  margin: 40px;
+  box-sizing: border-box;
+  display: flex;
+`;
+
+export const PhotoDiv = styled.div`
+  height: 500px;
+  width: 40%;
+  background-color: white;
+`;
+
+export const TextDiv = styled.div`
+  color: white;
+  background-color: black;
+  width: 60%;
+`;
