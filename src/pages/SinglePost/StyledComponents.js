5e8befeea437adc4a2ad7d6386d2d1e36ff750ee
Owen J Sullivan <multiojuice@gmail.com>
2018-11-06 19:20:35 -0800

    Changed the font and formatted the markdown area

--- src/pages/SinglePost/StyledComponents.js
+++ src/pages/SinglePost/StyledComponents.js
@@ -22,12 +22,44 @@ export const TitleWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   
-  h1 {font-size: 2em;}
+  h1 {
+    font-weight: 100;
+    font-size: 52px;
+    text-shadow: 1px 1px 1px #ccc;
+  }
+
+  h3 {
+    font-weight: 100;
+    font-size: 18px;
+    font-style: italic;
+    margin-top: 0;
+
+  }
 `;
 
 export const ContentWrapper = styled.div`
-  background-color: #f7f7f7;
-  margin: 0 15%;
+  background-color: #f7f7ff;
+  margin: 0 15% 60px 15%;
   padding: 30px 40px;
   z-index: 1;
+  border-radius: 3px;
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Corben', Georgia, Times, serif;
+    font-weight: 100;
+    margin: 10px 0;
+  }
+
+  h5 {
+    font-size: 24px;
+  }
+
+  p {
+    color: grey;
+    font-style: inherit;
+  }
+
+  blockquote {
+    font-style: italic !important;
+  }
 `;
