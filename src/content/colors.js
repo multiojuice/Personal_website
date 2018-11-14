aefc51f5d94bafcc1829d1b7a8df6c4673efd22e
Owen J Sullivan <multiojuice@gmail.com>
2018-11-13 23:33:27 -0800

    Pulled out colors to seperate file so I can use a universal color scheme.

---
+++ src/content/colors.js
@@ -0,0 +1,6 @@
+export const darkblue = '#005995';
+export const white = '#ffff99';
+export const purple = '#754C78';
+export const red = '#EE6363';
+export const darkgrey = '#4B4544';
+export const yellow = '#ffff99';
--- src/pages/SinglePost/StyledComponents.js
+++ src/pages/SinglePost/StyledComponents.js
@@ -1,4 +1,12 @@
 import styled from 'styled-components';
+import {
+  red,
+  darkblue,
+  white,
+  purple,
+  darkgrey,
+  yellow
+} from '../../content/colors'
 
 export const PageWrapper = styled.div`
   display: flex;
@@ -11,10 +19,10 @@ export const HeaderWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: flex-end;
-  height: 40vh;
+  justify-content: flex-start;
+  height: 20vh;
   width: 100%;
-  background-color: #C23F38;
+  background-color: ${red};
   margin-bottom: 120px;
 `;
 
@@ -24,42 +32,38 @@ export const TitleWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 30px;
-  background-color: #600473;
-  width: 70%;
+  background-color: ${purple};
+  width: 80%;
   height: 40%;
   justify-content: center;
   border-radius: 3px;
 
   h1 {
     font-weight: 100;
-    color: #F5FAFA;
+    color: ${white};
     font-size: 52px;
-    text-shadow: 1px 1px 1px #6D929B;
+    text-shadow: 1px 1px 1px ${darkblue};
   }
 
   h3 {
     font-weight: 100;
     font-size: 22px;
     margin-top: 0;
-    color: #6D929B;
+    color: ${white};
   }
 `;
 
 export const ContentWrapper = styled.div`
-  background-color: #f7f7ff;
+  background-color: ${white};
   margin: 0 15% 60px 15%;
   padding: 30px 40px;
   z-index: 1;
   border-radius: 3px;
-  border-left: 40px solid #005995;
-
-  h1, h2, h3, h4, h5, h6 {
-    font-weight: 100;
-    margin: 20px 0;
-  }
+  border-left: 40px solid ${darkblue};
+  color: ${darkgrey};
 
   h5 {
-    color: #005995;
+    color: ${darkgrey};
     font-size: 18px;
     font-family: 'monospace';
     margin-top: 30px;
@@ -72,18 +76,26 @@ export const ContentWrapper = styled.div`
   h3 {
     font-family: "Segoe UI", Frutiger, "Frutiger Linotype", "Dejavu Sans", "Helvetica Neue", Arial, sans-serif;
     font-size: 24px;
-    color: #005995;
+    color: ${darkgrey};
+    font-weight: 500;
   }
 
   p {
     font-family: 'monospace';
-    color: #600473;
+    color: inherit;
     font-style: inherit;
     font-size: 18px;
     line-height: 30px;
   }
 
   blockquote {
-    font-style: italic !important;
+    color: ${yellow};
+    font-style: italic !impo3rtant;
+    background-color: ${darkgrey};
+    padding: 20px;
+    font-weight: 600;
+    border-radius: 5px;
+    border-left: 18px solid ${red};
+    border-bottom: 24px solid ${red};
   }
 `;
